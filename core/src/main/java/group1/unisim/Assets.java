a8f93f384793c77403498d56e2e17bc8c002d2f8
Yawshi <12boscolau@gmail.com>
2024-12-10 21:07:40 +0000

    implemented events display
    added code for events display
    event display shown when an event happens, and for the next 30 seconds after it happens. hidden otherwise
    added event display background

--- core/src/main/java/group1/unisim/Assets.java
+++ core/src/main/java/group1/unisim/Assets.java
@@ -18,6 +18,7 @@ public class Assets {
     public static final String END_SCREEN = asset("endScreen.png");
     public static final String ACCOMMODATION_TEXTURE = asset("accom.png");
     public static final String THOUGHT_BACKGROUND = asset("thoughtBackground.png");
+    public static final String EVENT_BACKGROUND = asset("eventBackground.png");
 
     private static String asset(String asset) {
         ALL_ASSETS.add(asset);
