b4fd64d22182e0763f39452549c3892c79570efd
Alyx BB <git@alyxbb.tech>
2024-12-31 23:23:45 +0000

    this time the test actually passes

--- headless/src/test/java/group1/unisim/test/TimerTest.java
+++ headless/src/test/java/group1/unisim/test/TimerTest.java
@@ -42,6 +42,7 @@ public class TimerTest {
     public void testTimerDisplay() {
         Timer timer = new Timer(300);
         assertEquals("5:00",timer.toString());
+        timer.togglePause();
         timer.update(10);
         assertEquals("4:50",timer.toString());
     }
